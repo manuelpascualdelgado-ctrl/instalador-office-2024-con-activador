3e52b257606d10848adc6321c32dab8ea5fd8e16
manuelpascualdelgado-ctrl <manuelpascualdelgado@gmail.com>
2026-03-22 22:27:42 +0100

    Delete activacion.bat

--- activacion.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Abriendo PowerShell...
-
-powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "irm https://get.activated.win | iex"
-
-echo.
-echo Proceso terminado.
-pause
